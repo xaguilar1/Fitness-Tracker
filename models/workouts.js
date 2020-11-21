b2cc4aa764d8b6e1abf275d7ca51dc1a7f76231b
xray <xyaguilar77@gmail.com>
2020-11-21 08:18:58 -0500

    fixed routes

--- models/workouts.js
+++ models/workouts.js
@@ -10,7 +10,7 @@ const WorkoutSchema = new Schema({
         {
             type: {
                 type: String,
-                enum: ['Weights', "Cardio"],
+                enum: ['Resistance', "Cardio"],
                 required: true
             },
             name: {
