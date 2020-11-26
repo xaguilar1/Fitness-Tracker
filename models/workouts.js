3ce7e1e13d47794422f3f319ed303df5f6b3849f
Xyra Arroyo <xyaguilar77@gmail.com>
2020-11-26 10:39:27 -0500

    Update and rename connection.js to workouts.js
    rename file and entered a workout required type

--- models/workouts.js
+++ models/workouts.js
@@ -7,36 +7,38 @@ const workoutSchema = new Schema({
     exercises: [{
         type: {
             type: String,
+            trim: true,
+            required: [true, "Enter an exercise type!"]
         },
         name: {
             type: String,
             trim: true,
-            required: [true, "Please give an exercise name"]
+            required: [true, "Enter an exercise name!"]
         },
         duration: {
             type: Number,
             trim: true,
-            required: [true, "Please give an exercise duration"]
+            required: [true, "Enter duration of exercise!"]
         },
         weight: {
             type: Number,
             trim: true,
-            required: [true, "Please give a weight estimate"]
+            required: [true, "Enter weight #!"]
         },
         reps: {
             type: Number,
             trim: true,
-            required: [true, "Please give a number of reps"]
+            required: [true, "Enter # of reps!"]
         },
         sets: {
             type: Number,
             trim: true,
-            required: [true, "Please give a number of sets"]
+            required: [true, "Enter # of sets!"]
         },
         distance: {
             type: Number,
             trim: true,
-            required: [true, "Please give a number of miles"]
+            required: [true, "Enter # of miles!"]
         },
     }],
 }, {
